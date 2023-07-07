8f6366305336fdccf9f2d506d0302a6fc7087ee3
Shaik Saifuddin <sh450956@dal.ca>
2023-07-07 17:17:18 -0300

    changed port number

--- A3/index.js
+++ A3/index.js
@@ -5,6 +5,6 @@ const app = express();
 app.use(express.json());
 app.use("", routes);
 
-app.listen(8080, function () {
-    console.log("Server started at 8080");
+app.listen(8081, function () {
+    console.log("Server started at 8081");
   });
