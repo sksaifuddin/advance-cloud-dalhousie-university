13d0cede5caf65390ec321c18fdb785712774266
Shaik Saifuddin <sh450956@dal.ca>
2023-07-07 17:45:19 -0300

    added query to insert items

--- A3/routes/routes.js
+++ A3/routes/routes.js
@@ -5,8 +5,10 @@ const db = require('../database');
 
 router.post('/store-products', (req, res) => {
     console.log(req.body);
-    const sql = "INSERT INTO products (name, price, availability) VALUES ('test', 'test', true)";
-    db.query(sql, function (err, result) {
+    const body = req.body;
+    const sql = "INSERT INTO products (name, price, availability) VALUES ?";
+    const items = body.map(obj => Object.values(obj))
+    db.query(sql,[items], function (err, result) {
         if (err) throw err;
         console.log('result', result);
         console.log("1 record inserted");
