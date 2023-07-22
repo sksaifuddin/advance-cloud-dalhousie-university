e6ba1c901445ebf68e14ca76f42cc78ab72e067d
Shaik Saifuddin <sh450956@dal.ca>
2023-07-22 15:08:17 -0300

    created a new service for db generator

---
+++ term-assignment/backend/db-generator/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const routes = require("./routes/routes");
+
+const app = express();
+app.use(express.json());
+app.use("", routes);
+
+app.listen(8080, function () {
+    console.log("Server started at 80");
+});
---
+++ term-assignment/backend/db-generator/routes/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/test', (req, res) => {
+    res.send('Hello world')
+})
+
+module.exports = router;
