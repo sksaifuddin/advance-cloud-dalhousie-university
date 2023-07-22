c1a05ae82757b47dc1879916326a72efc96edd20
Shaik Saifuddin <sh450956@dal.ca>
2023-07-22 17:55:54 -0300

    complete generating table backend code

---
+++ term-assignment/backend/db-generator/components/generate-db.js
@@ -0,0 +1,46 @@
+const db = require("../database");
+const { v4: uuid } = require("uuid");
+
+const generateDb = async (userId, dbName, columns) => {
+  const res = await createNewUserTable(dbName, columns);
+  await insertDBdetails(userId, dbName);
+  return res;
+};
+
+const createNewUserTable = (dbName, columns) => {
+  return new Promise((resolve, reject) => {
+    const columnStrings = columns.map(
+      (column) =>
+        `${column.name} ${
+          column.type === "VARCHAR" ? "VARCHAR(255)" : column.type
+        }`
+    );
+    const sql = `CREATE TABLE ${dbName} (${columnStrings.join(", ")})`;
+    db.query(sql, (err, result) => {
+      if (err) throw err;
+      resolve({
+        statusCode: 200,
+        body: result,
+        message: `${dbName} database created`,
+      });
+    });
+  });
+};
+
+const insertDBdetails = (dbName, userId) => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      `INSERT INTO users_databases 
+            (database_id, database_name, user_id) 
+            VALUES ('${uuid()}', '${dbName}', '${userId}')`,
+      (err, res) => {
+        if (err) throw err;
+        resolve(res);
+      }
+    );
+  });
+};
+
+module.exports = {
+  generateDb,
+};
--- term-assignment/backend/db-generator/routes/routes.js
+++ term-assignment/backend/db-generator/routes/routes.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const { generateDb } = require('../components/generate-db');
 
-router.get('/test', (req, res) => {
-    res.send('Hello world')
+/**
+ * {
+ * userId: string,
+ * dbName: string,
+ * columns: [
+ *  {name: string, type: string (give type name according to mysql standards)}
+ * ]
+ * }
+ */
+router.post('/generate-db', async (req, res) => {
+    const { userId, dbName, columns } = req.body;
+    const newDatabseName = `${userId}_${dbName}`;
+    const response = await generateDb(userId, newDatabseName, columns)
+    res.send(response);
 })
 
 module.exports = router;
